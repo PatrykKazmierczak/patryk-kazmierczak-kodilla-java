d0d790d7ae9dae8f7c8f7fef64e8b5692599905d
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2019-04-10 10:59:13 +0200

    TestingMainCalculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -3,6 +3,8 @@ package com.kodilla.testing;
 import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
+import java.util.Objects;
+
 public class TestingMain {
     public static void main(String[] args){
         SimpleUser simpleUser = new SimpleUser("theForumUser");
@@ -17,9 +19,20 @@ public class TestingMain {
         // tu umieść kolejny test ->
         System.out.println("Test - pierwszy test jednostkowy:");
 
-        Calculator calculator = new Calculator(8, 4);
-        System.out.println(calculator.add());
-        System.out.println(calculator.substract());
+        Calculator calculator = new Calculator(8,4);
 
+        int result1 = calculator.add();
+        int result2 = calculator.substract();
+
+        if(Objects.equals(result1, 12)) {
+            System.out.println("test ok");
+        }else{
+            System.out.println("error");
+        }
+        if(Objects.equals(result2, 32)) {
+            System.out.println("test ok");
+        }else{
+            System.out.println("error");
+        }
     }
 }
