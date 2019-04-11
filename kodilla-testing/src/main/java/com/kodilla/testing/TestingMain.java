1b175c2e16f01f35bff31cfa9a1539ad4eec66b9
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2019-04-11 11:53:58 +0200

    CollectionTestSuite

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,38 +1,6 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
-
-import java.util.Objects;
-
 public class TestingMain {
-    public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUserName();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-        // tu umieść kolejny test ->
-        System.out.println("Test - pierwszy test jednostkowy:");
-
-        Calculator calculator = new Calculator(8,4);
-
-        int result1 = calculator.add();
-        int result2 = calculator.substract();
-
-        if(result1 == 12) {
-            System.out.println("test ok");
-        }else{
-            System.out.println("error");
-        }
-        if(result2 == 32) {
-            System.out.println("test ok");
-        }else{
-            System.out.println("error");
-        }
+    public static void main(String[] args) {
     }
 }
