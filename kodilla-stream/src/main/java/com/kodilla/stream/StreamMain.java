15c2202d6c947218eeb4feddbee299326bd2ad50
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2019-04-17 21:28:21 +0200

    Immutable

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,18 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
         PoemBeautifier beautify = new PoemBeautifier();
 
-        beautify.beautify("ABC",(s)->s.toUpperCase());
-        beautify.beautify("ABC",(s)->s.toLowerCase());
-        beautify.beautify("ABC",(s)->s.toUpperCase()+" " + "ABC");
-        beautify.beautify("ABC",(s)->s.toLowerCase()+" " + "ABC");
+        beautify.beautify("ABC", (s) -> s.toUpperCase());
+        beautify.beautify("ABC", (s) -> s.toLowerCase());
+        beautify.beautify("ABC", (s) -> s.toUpperCase() + " " + "ABC");
+        beautify.beautify("ABC", (s) -> s.toLowerCase() + " " + "ABC");
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
