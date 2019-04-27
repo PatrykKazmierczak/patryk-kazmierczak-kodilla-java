2557e75f595902f7d9594d261e26a3a888dc11c4
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2019-04-27 16:43:33 +0200

    RouteNotFoundException

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,31 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+
+public final class FlightFinder {
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        HashMap<String, Boolean> flights = new HashMap<>();
+        flights.put("Warsaw", true);
+        flights.put("Lodz", true);
+        flights.put("Krakow", false);
+
+        if (flights.containsKey(flight.getArrivalAirport())) {
+            throw new RouteNotFoundException("Arrival to airport doesn't exist");
+        }
+        if (flights.containsKey(flight.getDepartureAirport())) {
+            throw new RouteNotFoundException("Departure to airport doesn't exist");
+        }
+    }
+        public static void main(String[] args){
+
+            FlightFinder flightFinder = new FlightFinder();
+            try {
+                flightFinder.findFlight(new Flight("Warsaw", "Poznan"));
+            } catch (RouteNotFoundException e) {
+                System.out.println("Problem with the route: " + e.getMessage());
+            } finally {
+                System.out.println("Flight search");
+            }
+        }
+    }
+
