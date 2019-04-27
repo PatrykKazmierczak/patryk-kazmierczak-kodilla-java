1f1762bb59a0484174d3ffa51ac4443b84a0a5dd
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2019-04-27 15:33:58 +0200

    ExceptionHandling

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsGame.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsGame.java
@@ -12,11 +12,29 @@ public class RpsGame {
                 return HUMAN;
             } else {
                 return BOTH;
-            } else if ( ... ) {
-           ...
+
+            } else if (userSelection == PAPER) {
+                if (computerSelection == SCISSORS) {
+                    return COMPUTER;
+                } else if (computerSelection == ROCK) {
+                    return HUMAN;
+                } else {
+                    return BOTH;
+
+                } else if (userSelection == SCISSORS) {
+                    if (computerSelection == ROCK) {
+                        return COMPUTER;
+                    } else if (computerSelection == PAPER) {
+                        return HUMAN;
+                    } else {
+                        return BOTH;
+                    }
+                }
             }
-        ...
         }
+        return null;
+    }
+
         Winner winner = selectWinner(computerSelection, userSelection);
             if (winner == COMPUTER) {
                 computerScore++;
