719b19ed863a2385a1813f75218605edaf293ab7
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2019-05-29 18:46:28 +0200

    MoveStore

--- kodilla-patterns/src/main/java/chellenges/MoveStoreMain.java
+++ kodilla-patterns/src/main/java/chellenges/MoveStoreMain.java
@@ -6,7 +6,6 @@ public class MoveStoreMain {
     public static void main(String[] args){
         MoveStore moveStore = new MoveStore ();
         System.out.println (moveStore.getMovies ());
-
         System.out.println (moveStore.getMovies ().entrySet ().stream ()
                 .flatMap (entry->entry.getValue ().stream())
                 .collect(Collectors.joining("!")));
